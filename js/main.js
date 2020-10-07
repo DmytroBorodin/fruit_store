b3268f448d4287e7aa2a10e9b25b948ad64f14a3
Dmytro Borodin <48290702+DmytroBorodin@users.noreply.github.com>
2020-10-07 12:46:51 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+let popup = document.querySelector('.menu-popup');
+let body = document.querySelector('body');
+let closeBlock =document.querySelector('.close-block');
+let burgerBtn = document.querySelector('.menu-btn');
+let closeBtn = document.querySelector('.close-btn');
+
+burgerBtn.addEventListener('click', () => {
+	popup.classList.add('active');
+	body.classList.add('locked');
+});
+
+closeBlock.addEventListener('click', () => {
+	popup.classList.remove('active');
+	body.classList.remove('locked');
+});
+
+closeBtn.addEventListener('click', () => {
+	popup.classList.remove('active');
+	body.classList.remove('locked');
+});
